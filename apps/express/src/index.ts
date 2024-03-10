e7513893a421258d7a754c6e0989393f757bb452
Anthony Shew <anthonyshew@gmail.com>
2024-03-10 12:37:39 -0700

    Some little stuff.

--- apps/express/src/index.ts
+++ apps/express/src/index.ts
@@ -1,10 +1,11 @@
-import express, { Request, Response } from 'express';
+import type { Request, Response } from 'express';
+import express from 'express';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, TypeScript Express!');
+  res.send('Hello, TypeScript Expresss!');
 });
 
 app.listen(port, () => {
