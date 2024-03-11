7afb6af7d06e8e3c8efc81c7ceccc4aa1efb0d98
Einsight <ghaithkhalil2004@gmail.com>
2024-03-10 23:34:39 -0400

    updates

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -2,8 +2,6 @@ import { env } from "env";
 
 import { server } from "@seegull/wizard";
 
-server.listen({ port: 5000, host: "0.0.0.0" }, () => {
+server.listen({ port: env.SERVER_PORT, host: "0.0.0.0" }, () => {
   console.log("server listening");
-  // console.log(env.SERVER_PORT);
-  console.log(env.SERVER_PORT);
 });
